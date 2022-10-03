b2d9499a83d4d354134552bb9b0dac356b3aa433
tarık keskin <>
2022-10-04 00:00:01 +0300

    Bottom Navigation Bar

--- app/src/main/java/com/gulhan/travelguideapp/MainActivity.kt
+++ app/src/main/java/com/gulhan/travelguideapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.gulhan.travelguideapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.gulhan.travelguideapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navigation_host_fragment) as NavHostFragment
+
+        NavigationUI.setupWithNavController(binding.bottomNav,navHostFragment.navController)
+
     }
 }
