eb855304fc03d0a1837c49d1b6e15c29836ad19e
tarık keskin <>
2022-10-08 23:10:27 +0300

    Search on Trips

--- app/src/main/java/com/gulhan/travelguideapp/presentations/search/SearchFragment.kt
+++ app/src/main/java/com/gulhan/travelguideapp/presentations/search/SearchFragment.kt
@@ -2,17 +2,17 @@ package com.gulhan.travelguideapp.presentations.search
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.gulhan.travelguideapp.R
 import com.gulhan.travelguideapp.databinding.FragmentSearchBinding
 import com.gulhan.travelguideapp.domain.model.allList.TravelsItem
-import com.gulhan.travelguideapp.presentations.home.HomeFragmentViewModel
-import com.gulhan.travelguideapp.presentations.home.adapter.HomeCardAdapter
 import com.gulhan.travelguideapp.presentations.search.adapter.NearbyAttractionCardAdapter
 import com.gulhan.travelguideapp.presentations.search.adapter.TopDestinationCardAdapter
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,6 +23,7 @@ class SearchFragment : Fragment() {
     private lateinit var binding : FragmentSearchBinding
     private lateinit var adapter: TopDestinationCardAdapter
     private lateinit var adapter2: NearbyAttractionCardAdapter
+    private lateinit var adapterSearch: NearbyAttractionCardAdapter
     private val travelViewModel by viewModels<SearchFragmentViewModel>()
 
     override fun onCreateView(
@@ -33,22 +34,45 @@ class SearchFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_search,container,false)
 
 
-        val list: MutableList<TravelsItem> = ArrayList()
-        val list2: MutableList<TravelsItem> = ArrayList()
+        val listTop: MutableList<TravelsItem> = ArrayList()
+        val listNearby: MutableList<TravelsItem> = ArrayList()
         travelViewModel.getAllTravels().observe(viewLifecycleOwner){
-            it.forEach{ flight ->
-                if(flight.category == "topdestination"){
-                    list.add(flight)
+            it.forEach{ trip ->
+                if(trip.category == "topdestination"){
+                    listTop.add(trip)
                 }
-                if(flight.category == "nearby"){
-                    list2.add(flight)
+                if(trip.category == "nearby"){
+                    listNearby.add(trip)
                 }
             }
-            adapter = TopDestinationCardAdapter(requireContext(),list)
-            adapter2 = NearbyAttractionCardAdapter(requireContext(),list2)
+            adapter = TopDestinationCardAdapter(requireContext(),listTop)
+            adapter2 = NearbyAttractionCardAdapter(requireContext(),listNearby)
             binding.rvTopDestinations.adapter = adapter
             binding.rVNearby.adapter = adapter2
         }
+
+        //Search
+        val listAll: MutableList<TravelsItem> = ArrayList()
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                travelViewModel.getAllTravels().observe(viewLifecycleOwner){
+                    it.forEach{ trip ->
+                        if(trip.city.contains(p0!!,true)){
+                            listAll.add(trip)
+                        }
+                    }
+                    binding.cLNotSearched.visibility = View.GONE
+                    binding.cLSearched.visibility = View.VISIBLE
+                    adapterSearch = NearbyAttractionCardAdapter(requireContext(),listAll)
+                    binding.rVSearched.adapter = adapterSearch
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+        })
 
 
         return binding.root
